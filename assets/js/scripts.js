706d567484e37d757bc36578040ef4d26e43afca
Ana <anabia200113@gmail.com>
2022-06-29 18:50:03 -0300

    primeira versao

---
+++ assets/js/scripts.js
@@ -0,0 +1,14 @@
+var currentNumberWrapper = document.getElementById("currentNumber");
+var currentNumber = 0;
+
+function increment() {
+    currentNumber = currentNumber + 1;
+    currentNumberWrapper.innerHTML = currentNumber;
+}
+
+function decrement() {
+    currentNumber = currentNumber - 1;
+    currentNumberWrapper.innerHTML = currentNumber;
+}
+
+// mude a cor do texto em current_number p/ vermelho quando o número for neg.
